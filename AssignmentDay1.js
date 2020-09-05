4bb8a8826beab5de19de9d4e86a51572dc02b70c
vrushalivp <59498744+vrushalivp@users.noreply.github.com>
2020-09-05 17:30:46 +0530

    Add files via upload

---
+++ AssignmentDay1.js
@@ -0,0 +1,55 @@
+// Program  to find particular character in String
+console.log("-----------Question 1-----------")
+let string1="Welcome";
+console.log("Output : \t" + string1);
+console.log(string1.indexOf("c"))
+
+/* output :
+    Welcome
+    3
+*/
+
+
+ // Write a program to convert minutes to seconds
+ console.log("\n-----------Question 2-----------")
+ let  minutes = 15 ;
+ let seconds = minutes * 60;
+ console.log("Output : \t seconds : " + seconds);
+
+ /* output :
+   seconds : 900
+ */
+
+
+
+ // Program to search element in a array of strings
+ console.log("\n-----------Question 3-----------")
+ let arr = ["fb","snapchat","instagra","hike"];
+   console.log("Output : \t " + arr[2]);
+
+ /* output :
+     instagra
+ */
+
+
+
+ //Program to display only elements containing 'a' in the term of array
+ console.log("\n-----------Question 4-----------")
+var array2 =  ["and", "glass", "student", "Teacher"];
+//var abc = array2.Contains("a")
+
+//console.log(abc)
+
+ 
+
+
+
+ // Print an array in reverse order
+ console.log("\n-----------Question 5-----------");
+ var arrayRev = ["pen", "pencil", "eraser", "scale"];
+ console.log("Output : \t"+ arrayRev.reverse())
+
+/*
+Output:
+scale,eraser,pencil,pen
+*/
